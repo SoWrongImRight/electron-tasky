57ac4452c9610d685d98c1ddb5e0d34bfb2199c5
Russ Carroll <info@rcarroll.dev>
2022-10-03 16:59:44 -0400

    Working on tray app.

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const electron = require('electron');
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, 
+    },
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+  tray = new Tray(iconPath);
+  tray.on('click', (event, bounds) => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show();
+    }
+  })
+})
