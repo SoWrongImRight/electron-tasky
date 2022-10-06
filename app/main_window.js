dcaac94987920928b0b48dd7b9f4cf4cafd49bba
Russ Carroll <info@rcarroll.dev>
2022-10-05 22:57:56 -0400

    Setup the timer_tray and main_window classes.

---
+++ app/main_window.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+  constructor() {
+    super({
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false, 
+      },
+      height: 500,
+      width: 300,
+      frame: false,
+      resizable: false,
+      show: false,
+      skipTaskbar: true,
+    });
+
+    this.on('blur', this.onBlur.bind(this));
+  }
+
+  onBlur() {
+    this.hide();
+  }
+}
+
+module.exports = MainWindow
--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 const path = require('path');
 const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false, 
-    },
-    height: 500,
-    width: 300,
-    frame: false,
-    resizable: false,
-    show: false,
-  });
+  mainWindow = new MainWindow();
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-
+ 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-  tray = new Tray(iconPath);
-  tray.on('click', (event, bounds) => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide()
-    } else {
-      mainWindow.show();
-    }
-  })
-})
+  tray = new TimerTray(iconPath, mainWindow);
+});
